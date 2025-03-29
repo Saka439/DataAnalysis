52d11c3e31c0eb6e9c1d86ea8ad446778176bcef
Saka439 <ndzana-missia.saka@polymtl.ca>
2025-03-29 17:54:32 -0400

    Premier commit: devoir de MTH2302D (projet R)

---
+++ Analyse_IR.R
@@ -0,0 +1,31 @@
+#Histogramme de Tukey
+hist(mondata$IR, main="Histogramme de l'indice de Rugosité", xlab="IR", col = "skyblue",border = "black",xlim = c(0, max(mondata$IR)))
+
+#Diagramme de Tukey
+boxplot(mondata$IR, main="Boxplot de l'indice de Rugosité", xlab="IR" ,col = "purple", horizontal = TRUE)
+
+
+# Normal Probability Plot (droite de Henry)
+qqnorm(mondata$IR,main="Droite de Henry pour IR")
+qqline(mondata$IR, col="red")
+
+# Test de normalité de Shapiro-Wilk
+shapiro.test(mondata$IR)
+
+
+summary(mondata$IR)  # Moyenne, quartiles, min, max
+sd(mondata$IR)        # Écart-type
+mean(mondata$IR)      # Moyenne
+var(mondata$IR)       # Variance
+
+
+n <- length(mondata$IR)
+moyenne <- mean(mondata$IR)
+ecart_type <- sd(mondata$IR)
+se <- ecart_type / sqrt(n)  # Erreur standard
+alpha <- 0.05
+t_critique <- qt(1 - alpha/2, df=n-1)
+IC_lower <- moyenne - t_critique * se
+IC_upper <- moyenne + t_critique * se
+c(IC_lower, IC_upper)
+
