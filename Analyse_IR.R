499f8081a55f44adf207ccb214c7928e26ed301e
Saka439 <ndzana-missia.saka@polymtl.ca>
2025-03-31 10:02:39 -0400

    phase 1a terminé

--- Analyse_IR.R
+++ Analyse_IR.R
@@ -4,6 +4,7 @@ mondata <- charger(2184805)
 #Pour visualiser l'ensemble des donnees
 mondata
 
+
 #Histogramme de Tukey
 hist(mondata$IR,
      main = "Histogramme de l'indice de Rugosité",
@@ -13,25 +14,30 @@ hist(mondata$IR,
      border = "red",
      breaks = seq(0, 30, by = 1))
 
-#axis(1, at = seq(0, 30, by = 5))
+
 
 #Diagramme de Tukey
-boxplot(mondata$IR, main="Boxplot de l'indice de Rugosité", 
-xlab="Indice de Rugosité", col = "purple", horizontal = TRUE)
+boxplot(mondata$IR, main = "Boxplot de l'indice de Rugosité", 
+xlab = "Indice de Rugosité", col = "purple", horizontal = TRUE)
 
 
 # Normal Probability Plot (droite de Henry)
-qqnorm(mondata$IR,main="Droite de Henry pour IR")
-qqline(mondata$IR, col="red")
+qqnorm(mondata$IR, main = "Droite de Henry pour IR", col = "blue")
+qqline(mondata$IR, col = "red")
 
 # Test de normalité de Shapiro-Wilk
 shapiro.test(mondata$IR)
 
+moyenne <- mean(mondata$IR)
+mediane <- median(mondata$IR)
+variance <- var(mondata$IR)
+ecart_type <- sd(mondata$IR) 
+coefficient_variation <- ecart_type / variance
 
-summary(mondata$IR)  # Moyenne, quartiles, min, max
-sd(mondata$IR)        # Écart-type
-mean(mondata$IR)      # Moyenne
-var(mondata$IR)       # Variance
+cat('moyenne = ', moyenne, ', médiane = ', mediane, ', variance = ', variance ,', écart-type = ', ecart_type, ', coefficient de variation = ', coefficient_variation)
+
+summary(mondata$IR)  
+
 
 
 n <- length(mondata$IR)
@@ -39,8 +45,8 @@ moyenne <- mean(mondata$IR)
 ecart_type <- sd(mondata$IR)
 se <- ecart_type / sqrt(n)  # Erreur standard
 alpha <- 0.05
-t_critique <- qt(1 - alpha/2, df=n-1)
-IC_lower <- moyenne - t_critique * se
-IC_upper <- moyenne + t_critique * se
-c(IC_lower, IC_upper)
+t_critique <- qt(1 - alpha / 2, df = n - 1)
+ic_lower <- moyenne - t_critique * se
+ic_upper <- moyenne + t_critique * se
+c(ic_lower, ic_upper)
 
